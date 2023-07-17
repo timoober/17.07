e72ff1a8ea718f09bb547d86ff7ee0eb20897693
timoober <timo.oberschachtsiek@gmail.com>
2023-07-17 09:33:51 +0200

    v2

---
+++ backend/model/TodosModel.js
@@ -0,0 +1,5 @@
+import fs from "node:fs/promises"
+import {v4 as uuidv4} from "uuid"
+
+const File_path = "./data/todos.json"
+
--- backend/server.js
+++ backend/server.js
@@ -6,3 +6,16 @@ const PORT = 3001;
 
 app.use(express.json());
 app.use(cors());
+
+
+app.get("/api/todos", (req, res)=> {
+    res.send("todos werden hier angezeigt")
+})
+
+app.post("/api/todos", (req, res)=> {
+    res.send("todo wurde hinzugefügt")
+})
+
+
+
+app.listen(PORT, ()=> {console.log(`Server ist am laufen auf diesem Port${PORT}`)})
